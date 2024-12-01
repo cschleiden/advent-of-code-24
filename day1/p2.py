a13160e86080596fc37ded5beea550afa4a97626
cschleiden <cschleiden@live.de>
2024-12-01 13:03:17 -0800

    part 2

--- day1/p2.py
+++ day1/p2.py
@@ -8,16 +8,21 @@ with open('day1/input.txt', 'r') as file:
     list1.append(int(parts[0]))
     list2.append(int(parts[1]))
 
-# Sort lists
-list1.sort()
-list2.sort()
+
+l2counts = {}
+
+for l2 in list2:
+  if l2 in l2counts:
+    l2counts[l2] += 1
+  else:
+    l2counts[l2] = 1
 
 sum = 0
 
 # Iterate through the lists
 for i in range(len(list1)):
   l1 = list1[i]
-  l2 = list2[i]
-  sum += abs(l1 - l2)
+  if l1 in l2counts:
+    sum += l1 * l2counts[l1]
 
 print(sum)
