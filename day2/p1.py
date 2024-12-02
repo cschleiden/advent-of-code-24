2c106f2315026a8a1ea4f42f6456693ce06a7497
cschleiden <cschleiden@live.de>
2024-12-01 22:02:21 -0800

    brute force p2

--- day2/p1.py
+++ day2/p1.py
@@ -1,17 +1,16 @@
-def read_input(path):
-    reports = []
-    with open(path, 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-            levels = line.split()
-            reports.append(levels)
-    return reports
+from typing import List
 
-reports = read_input('day2/input.txt')
+def read_input(path: str) -> List[List[int]]:
+  reports = []
+  with open(path, 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+      levels = line.split()
+      levels = list(map(int, levels))
+      reports.append(levels)
+  return reports
 
-safe_reports = 0
-
-for report in reports:
+def safe_report(report):
     prev = int(-1)
     increasing = False
     safe = True
@@ -31,8 +30,25 @@ for report in reports:
             safe = False
             break
       prev = level
+    return safe
+
+
+reports = read_input('day2/input.txt')
+
+safe_reports = 0
+
+for report in reports:
+    safe = safe_report(report)
     if safe:
         safe_reports += 1
+    else:
+        for i in range(len(report)):
+          # new report without i
+          new_report = report[:i] + report[i+1:]
+          safe = safe_report(new_report)
+          if safe:
+            safe_reports += 1
+            break
 
 
 print(safe_reports)
