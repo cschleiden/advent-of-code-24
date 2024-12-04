7d27e70bef018b7a0001b8920c603b00740d2f1c
cschleiden <cschleiden@live.de>
2024-12-03 20:37:02 -0800

    day 3 - part 2

---
+++ day3/p2.py
@@ -0,0 +1,46 @@
+ops = []
+
+do = True
+
+with open('day3/input.txt', 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        for i in range(len(line)):
+            if ''.join(line[i:i+4]) == 'mul(':
+                i += 4
+                n1 = []
+                while line[i].isdigit():
+                    n1.append(line[i])
+                    i += 1
+                if line[i] == ',':
+                    i += 1
+                else:
+                    continue
+                n2 = []
+                while line[i].isdigit():
+                    n2.append(line[i])
+                    i += 1
+                if line[i] == ')':
+                    i += 1
+                else:
+                    continue
+                if do:
+                    ops.append([n1, n2])
+            elif ''.join(line[i:i+4]) == 'do()':
+                i += 4
+                do = True
+            elif ''.join(line[i:i+7]) == "don't()":
+                i += 7
+                do = False
+            else:
+                i += 3
+
+s = 0
+
+for ops in ops:
+    n1 = int(''.join(ops[0]))
+    n2 = int(''.join(ops[1]))
+    print(n1 * n2)
+    s += n1 * n2
+
+print(s)
